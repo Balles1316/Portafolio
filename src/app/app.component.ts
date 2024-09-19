642fe30bae83ee4657218d37f5cf1288c2b59b9a
Balles <romanballesteros8@Hotmail.com>
2024-09-19 12:47:33 +0200

    Frist Angular Proyect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {dateTimestampProvider} from "rxjs/internal/scheduler/dateTimestampProvider";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  name = 'Román Ballesteros';
+  description = 'Full Stack Developer passionate about building web applications.';
+  email = 'romanballesteros@hotmail.com';
+  github = 'https://github.com/Balles1316';
+  linkedin = 'romanballesteros@hotmail.com';
+  projects = ['Project 1', 'Project 2', 'Project 3'];
   title = 'untitled';
+  currentYear: number = new Date().getFullYear();
 }
